792d35872361ff4e9d5f8b6803c7063f898f88e0
Shakib <s628561@gmail.com>
2025-08-28 15:27:38 +0600

    Environment Setup

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+    base: '/B12-A5-Emergency-Hotline/',
+    plugins: [
+        tailwindcss(),
+    ],
+})
