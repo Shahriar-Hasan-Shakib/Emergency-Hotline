10d580f5661063388a953f5e633f715f8a95ef89
Shakib <s628561@gmail.com>
2025-08-28 17:30:48 +0600

    Responsive design and Final Polish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-    base: '/B12-A5-Emergency-Hotline/',
+    base: '/Emergency-Hotline/',
     plugins: [
         tailwindcss(),
     ],
